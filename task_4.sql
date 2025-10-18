1d7b771d5d97148dc10f2455360c8a2240bad077
Jeremiah <jeremensz@gmail.com>
2025-10-18 12:39:15 +0000

    Add task_4.sql: full description of Books table using INFORMATION_SCHEMA

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
 -- task_4.sql
--- Prints the full description of the table 'books' in alx_book_store
+-- Prints the full description of the table 'Books' in alx_book_store
 
 USE alx_book_store;
 
@@ -12,4 +12,5 @@ SELECT
     EXTRA AS 'Extra'
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
+  AND TABLE_NAME = 'Books';
+
