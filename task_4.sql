254d7bbf82e5d88af924d67ea608ea38990277b6
Jeremiah <jeremensz@gmail.com>
2025-10-19 21:42:12 +0000

    Added task_4.sql to show full description of books table without using DESCRIBE or EXPLAIN

---
+++ task_4.sql
@@ -0,0 +1,12 @@
+-- Script to display full description of 'books' table without using DESCRIBE or EXPLAIN
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
